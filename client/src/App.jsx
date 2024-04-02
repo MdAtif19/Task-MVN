0db7618d8a14f310b5a5360ff300fb61f2c2b6f0
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-04-02 21:08:40 +0600

    React router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import react, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./index.css";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
